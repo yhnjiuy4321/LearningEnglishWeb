2782b8ae40224dabc0d363c0c2ec410a90f5a53f
yhnjiuy4321 <d1204360@o365.fcu.edu.tw>
2025-01-07 11:05:37 +0800

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base:'./',
+  plugins: [
+    vue(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))//解釋 這裡是指定src資料夾的位置 這樣就可以在其他地方使用@/components/xxx
+    }
+  }
+})
