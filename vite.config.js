11331bad61452eb2e694715f5e6ce568e51f7d47
yhnjiuy4321 <d1204360@o365.fcu.edu.tw>
2026-01-11 22:02:53 +0800

    20260111

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'./',
+  base:'/LearningEnglishWeb/', //設定部署的基礎路徑
   plugins: [
     vue(),
   ],
